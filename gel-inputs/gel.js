7bcb91d5dd51783a9a7928698f7748d9eea7e9ae
Liam Allen <liamallen343@gmail.com>
2023-07-18 16:05:15 -0500

    Added gel buttons

---
+++ gel-inputs/gel.js
@@ -0,0 +1,7 @@
+$(".toggle-button").on("click", () => {
+
+    $(".toggle-button").toggleClass("toggle-shadow")
+    $(".toggle-button-checkbox").toggleClass("toggle-border")
+    $(".toggle-button-extra").toggleClass("edited")
+    
+})
